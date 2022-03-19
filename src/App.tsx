cf6c86bf4c26a6710a940f90e3bf5d8b4d18c945
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-19 18:13:22 -0400

    add: miragejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { Container } from "./components/Header/styles";
-import { TransactionsTable } from "./components/TransactionsTable";
 import { GlobalStyle } from "./styles/global";
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createServer} from 'miragejs';
 import {App} from './App';
 
+// https://miragejs.com/
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () =>{
+      return [
+        {
+          id: 1,
+          title: 'Transactions',
+          amount: 500,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
