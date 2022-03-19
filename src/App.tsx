67b401ff48fc5884724314caf3612ef6521aaf45
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-19 08:47:26 -0400

    GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
+    <>
     <div className="App">
      <h1>Hello</h1>
+     <GlobalStyle />
     </div>
+    </>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+
+export function App(){
+  return(
+    <div className="App">
+      <h1 className="App">Hello</h1>
+    </div>
+  )
+}
