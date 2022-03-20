9efbe5bceacdfda59ff6d377f2b7fa06b8f14d16
Sergio Rosa <scarlosjazz@gmail.com>
2022-03-20 10:23:42 -0400

    add: NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { Container } from "./components/Header/styles";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 
@@ -24,15 +25,11 @@ export function App() {
   return (
     <>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-
      <Dashboard />
-
-     <Modal 
-      isOpen={isNewTransactionModalOpen} 
-      onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Register new Transaction</h2>
-      </Modal>
+     <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen} 
+     onRequestClose={handleCloseNewTransactionModal}
+     />
      <GlobalStyle />
      <Container />
     </>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,20 @@
+import Modal from 'react-modal';
+
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
+  return(
+    <Modal 
+      isOpen={isOpen} 
+      onRequestClose={ onRequestClose}
+      >
+      <h2>Register new Transaction</h2>
+    </Modal>
+  );
+}
+
+
